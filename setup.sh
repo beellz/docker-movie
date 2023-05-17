c161a4bbb5fdc68932dc3dd6da2087e92f03484a
sayed belal <sayed.b@greyorange.com>
2023-05-17 11:30:49 +0530

    writing a setup file to run them fast

---
+++ setup.sh
@@ -0,0 +1,4 @@
+docker-compose -f ./jellyfin/docker-compose.yaml up -d
+docker-compose -f ./samba/docker-compose.yaml up -d
+docker-compose -f ./transmission/docker-compose.yaml up -d
+docker-compose -f ./webpage/docker-compose.yaml up -d
